71927aff4f8851e3d5e8093917fd703b4ec70954
wangsheng <1748532294@qq.com>
2017-04-21 13:31:15 +0800

    4/21 标题栏搞定，推荐界面框架完成

---
+++ app/src/main/java/com/momo/imgrecognition/module/main/adapter/ImageAdapter.java
@@ -0,0 +1,28 @@
+package com.momo.imgrecognition.module.main.adapter;
+
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import com.momo.imgrecognition.module.main.bean.ImageBean;
+
+/**
+ * Created by Administrator on 2017/4/21.
+ */
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageBean> {
+    @Override
+    public ImageBean onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ImageBean holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/momo/imgrecognition/module/main/bean/ImageBean.java
@@ -0,0 +1,10 @@
+package com.momo.imgrecognition.module.main.bean;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Administrator on 2017/4/21.
+ */
+
+public class ImageBean extends RecyclerView.ViewHolder {
+}
