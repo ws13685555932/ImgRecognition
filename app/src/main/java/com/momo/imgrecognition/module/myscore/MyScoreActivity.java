64b0bc04df655e04020a1858e53525fb11bfe11f
ws13685555932 <1748532294@qq.com>
2017-05-22 23:01:20 +0800

    5/22 我的积分界面完成

--- app/src/main/java/com/momo/imgrecognition/module/myscore/MyScoreActivity.java
+++ app/src/main/java/com/momo/imgrecognition/module/myscore/MyScoreActivity.java
@@ -1,15 +1,30 @@
 package com.momo.imgrecognition.module.myscore;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.momo.imgrecognition.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MyScoreActivity extends AppCompatActivity {
+
+    @BindView(R.id.back)
+    ImageView back;
+    @BindView(R.id.title)
+    TextView title;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_score);
+        ButterKnife.bind(this);
+
     }
 }
